b30ca6e375a70d138d6fbf2f7ef49228de70ff3c
Safa <tosun.huseyinsafa@gmail.com>
2023-12-01 19:38:00 +0300

    restfulödevi

---
+++ PatikaRestfulApi/PatikaRestfulApi/Controllers/ProductsController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using PatikaRestfulApi.Entities.Models;
+using System.Globalization;
+
+namespace PatikaRestfulApi.Controllers
+{
+    [Route("api/products")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private static List<Product> _products = new List<Product>
+        {
+            new Product { Id = 1, Name = "Bilgisayar", Price = 2000 },
+            new Product { Id = 2, Name = "Televizyon", Price = 1000 },
+            new Product { Id = 3, Name = "Tablet", Price = 400 },
+        };
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            return Ok(_products);
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult GetOneProductById([FromRoute(Name = "id")] int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        [HttpGet("list")]
+        public IActionResult GetAllProductsByName([FromQuery] string name)
+        {
+            var Products = _products.Where(p => p.Name.Contains(name, StringComparison.OrdinalIgnoreCase)).OrderBy(p => p.Name);
+
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest();
+            }
+
+            return Ok(Products);    
+        }
+
+        [HttpPost]
+        public IActionResult CreateOneProduct([FromBody] Product product)
+        {
+            if (product is null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _products.Add(product);
+
+            return StatusCode(201, product);
+
+        }
+
+        [HttpPut("{id:int}")]
+        public IActionResult UpdateOneProduct([FromRoute(Name = "id")] int id, [FromBody] Product product)
+        {
+            if(id != product.Id)
+            {
+                return BadRequest();
+            }
+
+            var Product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (Product == null)
+            {
+                return NotFound(); 
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); 
+            }
+
+            Product.Name = product.Name;
+            Product.Price = product.Price;
+
+            return NoContent(); 
+        }
+
+        [HttpDelete("{id:int}")]
+        public IActionResult DeleteOneProduct([FromRoute(Name = "id")] int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();  
+            }
+
+            _products.Remove(product);
+
+            return NoContent();  
+        }
+
+        [HttpPatch("{id:int}")]
+        public IActionResult Patch([FromRoute(Name = "id")] int id, [FromBody] JsonPatchDocument<Product> productPatch)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+           
+            if (product == null)
+            {
+                return NotFound(); 
+            }
+
+            productPatch.ApplyTo(product, ModelState);
+            
+            TryValidateModel(product);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState); 
+            }
+
+            return NoContent(); 
+        }
+
+       
+
+    }
+}
---
+++ PatikaRestfulApi/PatikaRestfulApi/Entities/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PatikaRestfulApi.Entities.Models
+{
+    public class Product
+    {
+
+        [Required(ErrorMessage = "Id is a required field.")]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Name is a required field.")]
+        [MinLength(2, ErrorMessage = "Name must consist of a at least 2 characters")]
+        [MaxLength(40, ErrorMessage = "Name must consist of a at maximum 40 characters")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Price is a required field.")]
+        [Range(10, 10000)]
+        public decimal Price { get; set; }
+
+    }
+}
