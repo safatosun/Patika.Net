e31dabff7b8b0cdd4c1e1699da6afe1de65e9236
safatosun <110028593+safatosun@users.noreply.github.com>
2023-12-28 01:32:35 +0300

    Update homework2.sql

--- Sql/homework2.sql
+++ Sql/homework2.sql
@@ -1,11 +1,8 @@
+SELECT * FROM film
+WHERE replacement_cost BETWEEN '12.99' AND '16.99';
 
-SELECT country FROM country WHERE country LIKE 'A%a'
-
-SELECT country FROM country WHERE country LIKE '%_____n
-
-SELECT title
-FROM film
-WHERE LOWER(title) LIKE '%t%t%t%t%' OR UPPER(title) LIKE '%T%T%T%T%';
+SELECT * FROM actor
+WHERE first_name IN ('Penelope', 'Nick', 'Ed');
 
 SELECT * FROM film
-WHERE title LIKE 'C%'' AND length > '90' AND rental_rate = '2.99';
+WHERE rental_rate IN ('0.99', '2.99', '4.99') AND replacement_cost IN ('12.99', '15.99', '28.99');
